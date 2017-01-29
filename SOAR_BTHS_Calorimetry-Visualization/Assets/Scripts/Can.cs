3010ef5b9af380a3fb134778b04b0f8beddf9498
Peter Ferrarotto <Peter Ferrarotto>
2017-01-29 15:10:42 -0500

    Created framework of visualization - 10 steps, simple textures on tools/elements, limited interaction (only two buttons - interaction can be implemented more in a later build)

---
+++ SOAR_BTHS_Calorimetry-Visualization/Assets/Scripts/Can.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Can : MonoBehaviour {
+    public float emptyWeight;
+    public float weightWithWater;
+    public float initialTemp;
+    public float finalTemp;
+}
--- SOAR_BTHS_Calorimetry-Visualization/Assets/Scripts/StateManager.cs
+++ SOAR_BTHS_Calorimetry-Visualization/Assets/Scripts/StateManager.cs
@@ -4,37 +4,65 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StateManager : MonoBehaviour {
+    public string step1Description;
+    public string step2Description;
+    public string step3Description;
+    public string step4Description;
+    public string step5Description;
+    public string step6Description;
+    public string step7Description;
+    public string step8Description;
+    public string step9Description;
+    public string step10Description;
+
     public Text stepIndicator;
+    public Text stepDescriber;
+
+    public Text weightOfCandleBefore;
+    public Text weightOfCandleAfter;
+    public Text weightOfWaxBurned;
+
+    public Text weightOfCanEmpty;
+    public Text weightOfCanFull;
+    public Text weightOfWaterInCan;
+
+    public Text initialTemperature;
+    public Text temperatureAfter;
 
     public GameObject can;
-    public GameObject glassRod;
-    public GameObject ringStand;
     public GameObject candle;
+    public GameObject skewer;
     public Light candleLight;
     public GameObject chimney;
+    public GameObject balance;
 
     public Vector3 origin;
 
-    public Vector3 ringStandPosition0;
-    public Vector3 ringStandPosition1;
-
     public Vector3 canPosition0;
-    public Vector3 canPosition1;
-
-    public Vector3 rodPosition0;
-    public Vector3 rodPosition1;
+    public Vector3 canPosition2;
+    public Vector3 canPosition4;
+    public Vector3 canPosition6;
 
     public Vector3 candlePosition0;
     public Vector3 candlePosition1;
+    public Vector3 candlePosition4;
+    public Vector3 candlePosition6;
 
     public Vector3 chimneyPosition0;
-    public Vector3 chimneyPosition1;
+    public Vector3 chimneyPosition4;
+    public Vector3 chimneyPosition6;
+
+    public Vector3 balancePosition0;
+    public Vector3 balancePosition1;
 
     private int currentStep;
-    int finalStep = 4;
+
+    private float waterWeight = 0;
+    private float weightOfWax = 0;
+    int finalStep = 10;
 	// Use this for initialization
 	void Start () {
-        currentStep = 0;
+        currentStep = 1;
 
         candleLight.enabled = false;
 
@@ -58,7 +86,7 @@ public class StateManager : MonoBehaviour {
 
     public void decrementStep()
     {
-        if(currentStep > 0)
+        if(currentStep > 1)
         {
             currentStep--;
             updateObjects();
@@ -69,28 +97,106 @@ public class StateManager : MonoBehaviour {
     {
         switch (currentStep)
         {
-            case 0:
-                Debug.Log("Objects Updated to Step 0");
+            case 1:
                 candleLight.enabled = false;
                 chimney.transform.position = chimneyPosition0;
                 candle.transform.position = candlePosition0;
                 can.transform.position = canPosition0;
-                //glassRod.transform.position = rodPosition0;
-                ringStand.transform.position = ringStandPosition0;
-                break;
-            case 1:
-                ringStand.transform.position = ringStandPosition1;
-                can.transform.position = canPosition1;
-                //glassRod.transform.position = rodPosition1;
-                candle.transform.position = candlePosition1;
-                chimney.transform.position = chimneyPosition1;
-                candleLight.enabled = false;
+                balance.transform.position = balancePosition0;
+                skewer.SetActive(false);
+                stepDescriber.text = step1Description;
                 break;
             case 2:
+                candle.transform.position = candlePosition1;
+                chimney.transform.position = chimneyPosition0;
+                balance.transform.position = balancePosition1;
+                candleLight.enabled = false;
+                can.transform.position = canPosition0;
+                skewer.SetActive(false);
+                stepDescriber.text = step2Description;
+                weightOfCandleBefore.text = string.Concat(candle.GetComponent<Candle>().massBeforeBurn.ToString(), " g");
+                break;
+            case 3:
+                can.transform.position = canPosition2;
+                chimney.transform.position = chimneyPosition0;
+                candle.transform.position = candlePosition0;
+                balance.transform.position = balancePosition1;
+                candleLight.enabled = false;
+                skewer.SetActive(false);
+                stepDescriber.text = step3Description;
+                weightOfCanEmpty.text = string.Concat(can.GetComponent<Can>().emptyWeight.ToString(), " g");
+                break;
+            case 4:
+                candleLight.enabled = false;
+                candle.transform.position = candlePosition0;
+                skewer.SetActive(false);
+                chimney.transform.position = chimneyPosition0;
+                can.transform.position = canPosition2;
+                balance.transform.position = balancePosition1;
+                stepDescriber.text = step4Description;
+                weightOfCanFull.text = string.Concat(can.GetComponent<Can>().weightWithWater.ToString(), " g");
+                waterWeight = can.GetComponent<Can>().weightWithWater - can.GetComponent<Can>().emptyWeight;
+                weightOfWaterInCan.text = string.Concat(waterWeight.ToString(), " g");
+                break;
+            case 5:
+                can.transform.position = canPosition4;
+                balance.transform.position = balancePosition0;
+                chimney.transform.position = chimneyPosition4;
+                candle.transform.position = candlePosition4;
+                skewer.SetActive(true);
+                candleLight.enabled = false;
+                stepDescriber.text = step5Description;
+                initialTemperature.text = string.Concat(can.GetComponent<Can>().initialTemp.ToString(), " C");
+                break;
+            case 6:
+                skewer.SetActive(true);
                 candleLight.enabled = true;
+                chimney.transform.position = chimneyPosition4;
+                can.transform.position = canPosition4;
+                candle.transform.position = candlePosition4;
+                stepDescriber.text = step6Description;
+                break;
+            case 7:
+                candle.transform.position = candlePosition6;
+                can.transform.position = canPosition6;
+                balance.transform.position = balancePosition0;
+                chimney.transform.position = chimneyPosition6;
+                skewer.SetActive(true);
+                candleLight.enabled = true;
+                stepDescriber.text = step7Description;
+                break;
+            case 8:
+                skewer.SetActive(true);
+                candleLight.enabled = false;
+                stepDescriber.text = step8Description;
+                candle.transform.position = candlePosition6;
+                can.transform.position = canPosition6;
+                balance.transform.position = balancePosition0;
+                chimney.transform.position = chimneyPosition6;
+                temperatureAfter.text = string.Concat(can.GetComponent<Can>().finalTemp.ToString(), " C");
+                break;
+            case 9:
+                skewer.SetActive(false);
+                stepDescriber.text = step9Description;
+                chimney.transform.position = chimneyPosition0;
+                candle.transform.position = candlePosition1;
+                can.transform.position = canPosition0;
+                balance.transform.position = balancePosition1;
+                weightOfCandleAfter.text = string.Concat(candle.GetComponent<Candle>().massAfterBurn.ToString(), " g");
+                weightOfWax = candle.GetComponent<Candle>().massBeforeBurn - candle.GetComponent<Candle>().massAfterBurn;
+                weightOfWaxBurned.text = string.Concat(weightOfWax.ToString(), " g");
+                break;
+            case 10:
+                candleLight.enabled = false;
+                chimney.transform.position = chimneyPosition0;
+                candle.transform.position = candlePosition0;
+                can.transform.position = canPosition0;
+                balance.transform.position = balancePosition0;
+                skewer.SetActive(false);
+                stepDescriber.text = step10Description;
                 break;
             default:
-                currentStep = 0;
+                currentStep = 1;
                 updateObjects();
                 break;
         }
