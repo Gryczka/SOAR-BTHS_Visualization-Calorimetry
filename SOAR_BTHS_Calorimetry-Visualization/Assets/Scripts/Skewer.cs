d3d2d0f09039e906431ff56881c5fed08f628584
gryczka <gryczka@github.com>
2022-01-21 01:45:29 -0500

    chore/docs: cleaned up directories which should be gitignored and added README

--- SOAR_BTHS_Calorimetry-Visualization/Assets/Scripts/Skewer.cs
+++ SOAR_BTHS_Calorimetry-Visualization/Assets/Scripts/Skewer.cs
@@ -3,17 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Skewer : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void updateObject(int step)
     {
         switch (step)
